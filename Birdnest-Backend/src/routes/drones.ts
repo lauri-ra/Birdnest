5444a0aa9ef2fc0adac40de1ff8f29d475ca0594
lauri-ra <lrautiala+1@gmail.com>
2022-12-27 02:47:54 +0200

    Made getDrones more typesafe

--- Birdnest-Backend/src/routes/drones.ts
+++ Birdnest-Backend/src/routes/drones.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import droneService from '../services/droneService';
+import { Drone } from '../types';
 
 const router = express.Router();
 
 setInterval(async () => {
 	console.log('fetching...');
-	await droneService.getDrones();
+	const drones: Array<Drone> = await droneService.getDrones();
+
+	drones.forEach((drone) => {
+		droneService.checkTrespassing(drone.positionX, drone.positionY);
+	});
 }, 2000);
 
 router.get('/', (_request, response) => {
--- Birdnest-Backend/src/services/droneService.ts
+++ Birdnest-Backend/src/services/droneService.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { parseString } from 'xml2js';
+import { Drone } from '../types';
 
 // Check returned data and map it to an array or sth
-const getDrones = async () => {
+const getDrones = async (): Promise<Array<Drone>> => {
+	const jee: Array<Drone> = [];
+
 	const request = await axios.get('http://assignments.reaktor.com/birdnest/drones');
 
 	parseString(request.data as string, (error, result) => {
@@ -14,26 +17,26 @@ const getDrones = async () => {
 
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 			drones.forEach((drone: any) => {
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-				const id = drone.serialNumber[0];
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-				const positionX = drone.positionX[0];
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-				const positionY = drone.positionY[0];
+				const newDrone = {
+					serialNumber: drone.serialNumber[0],
+					positionY: drone.positionY[0],
+					positionX: drone.positionX[0],
+				};
 
-				console.log(id);
-				checkTrespassing(Number(positionX), Number(positionY));
+				jee.push(newDrone);
 			});
 		}
 	});
+
+	return jee;
 };
 
-const checkTrespassing = (x1: number, y1: number) => {
+const checkTrespassing = (x1: string, y1: string) => {
 	const x2 = 250000;
 	const y2 = 250000;
 	const r = 100000;
 
-	const distance: number = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+	const distance: number = Math.sqrt((x2 - Number(x1)) ** 2 + (y2 - Number(y1)) ** 2);
 
 	// Check if the point is inside the circle
 	if (distance < r) {
